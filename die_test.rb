d94e7fe3bdef43e1a27d6d65db2cb367153669d7
sai samant <saisamant@gmail.com>
2017-02-21 10:58:30 -0800

    added require statements

--- die_test.rb
+++ die_test.rb
@@ -1,1 +1,4 @@
 # die_test.rb
+require 'minitest/autorun' #require tells ruby that you are going to use these gems
+require 'minitest/reporters' #this one is optional (it adds color)
+require_relative 'die'  #seems to be ruby convention to use single-quotes for require statements
