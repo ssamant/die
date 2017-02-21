821328e0beab0430bd1c0373d8608164d14eff87
sai samant <saisamant@gmail.com>
2017-02-21 13:14:36 -0800

    added comments

--- die_test.rb
+++ die_test.rb
@@ -2,9 +2,11 @@
 require 'minitest/autorun' #require tells ruby that you are going to use these gems
 require 'minitest/reporters' #this one is optional (it adds color)
 Minitest::Reporters.use!
+
+
 require_relative 'die'  #seems to be ruby convention to use single-quotes for require statements
 
-class TestDie < Minitest::Unit::TestCase
+class TestDie < Minitest::Unit::TestCase #class TestDies is a set of Minitest unit tests
   def test_creation_of_die #all methods that are testing should start with word test
     new_die = Die.new
     assert new_die.class == Die, "A Die cannot be created as an instance of Die" #assert is a special method that takes 2 params. 1st is T/F, 2nd is optional
@@ -18,6 +20,10 @@ class TestDie < Minitest::Unit::TestCase
   def test_showing_number_must_be_greater_than_0
     new_die = Die.new
     assert new_die.showing > 0
+    # assert_operator die.showing, :>, 0 another assert method optional
+    # assert_nil die <-- the value of die should be nil
   end
+
+
 
 end
